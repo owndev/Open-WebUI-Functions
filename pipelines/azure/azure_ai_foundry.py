628dec15d1ab1662ccd1d47996261b905d9f4c78
ssweber <57631333+ssweber@users.noreply.github.com>
2025-03-21 03:02:02 -0400

    Add new Models (#13)
    * Add DeepSeek-V3
    
    * Add new Phi-4 and Mistral Small 3.1 Models
    
    ---------
    
    Co-authored-by: owndev <69784886+owndev@users.noreply.github.com>

--- pipelines/azure/azure_ai_foundry.py
+++ pipelines/azure/azure_ai_foundry.py
@@ -241,6 +241,7 @@ class Pipe:
             {"id": "Cohere-command-r-plus", "name": "Cohere Command R+"},
             {"id": "Cohere-command-r-plus-08-2024", "name": "Cohere Command R+ 08-2024"},
             {"id": "DeepSeek-R1", "name": "DeepSeek-R1"},
+            {"id": "DeepSeek-V3", "name": "DeepSeek-V3"},
             {"id": "jais-30b-chat", "name": "JAIS 30b Chat"},
             {"id": "Llama-3.2-11B-Vision-Instruct", "name": "Llama-3.2-11B-Vision-Instruct"},
             {"id": "Llama-3.2-90B-Vision-Instruct", "name": "Llama-3.2-90B-Vision-Instruct"},
@@ -256,6 +257,7 @@ class Pipe:
             {"id": "Mistral-Large-2411", "name": "Mistral Large 24.11"},
             {"id": "Mistral-Nemo", "name": "Mistral Nemo"},
             {"id": "Mistral-small", "name": "Mistral Small"},
+            {"id": "mistral-small-2503", "name": "Mistral Small 3.1"},
             {"id": "gpt-4o", "name": "OpenAI GPT-4o"},
             {"id": "gpt-4o-mini", "name": "OpenAI GPT-4o mini"},
             {"id": "o1", "name": "OpenAI o1"},
@@ -271,7 +273,9 @@ class Pipe:
             {"id": "Phi-3.5-mini-instruct", "name": "Phi-3.5-mini instruct (128k)"},
             {"id": "Phi-3.5-MoE-instruct", "name": "Phi-3.5-MoE instruct (128k)"},
             {"id": "Phi-3.5-vision-instruct", "name": "Phi-3.5-vision instruct (128k)"},
-            {"id": "Phi-4", "name": "Phi-4"}
+            {"id": "Phi-4", "name": "Phi-4"},
+            {"id": "Phi-4-mini-instruct", "name": "Phi-4 mini instruct"},
+            {"id": "Phi-4-multimodal-instruct", "name": "Phi-4 multimodal instruct"}
         ]
 
     def pipes(self) -> List[Dict[str, str]]:
