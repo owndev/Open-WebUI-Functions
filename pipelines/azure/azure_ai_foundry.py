7d4d4ae6f05bf267051c5a0c3aa152563929e5d1
owndev <69784886+owndev@users.noreply.github.com>
2025-04-24 09:35:21 +0200

    Add reasoning_effort

--- pipelines/azure/azure_ai_foundry.py
+++ pipelines/azure/azure_ai_foundry.py
@@ -4,7 +4,7 @@ author: owndev
 author_url: https://github.com/owndev
 project_url: https://github.com/owndev/Open-WebUI-Functions
 funding_url: https://github.com/owndev/Open-WebUI-Functions
-version: 2.2.0
+version: 2.2.1
 license: Apache License 2.0
 description: A pipeline for interacting with Azure AI services, enabling seamless communication with various AI models via configurable headers and robust error handling. This includes support for Azure OpenAI models as well as other Azure AI models by dynamically managing headers and request configurations.
 features:
@@ -379,6 +379,7 @@ class Pipe:
             "frequency_penalty",
             "max_tokens",
             "presence_penalty",
+            "reasoning_effort",
             "response_format",
             "seed",
             "stop",
