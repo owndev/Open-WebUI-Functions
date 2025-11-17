8b6631792aeed98630fe3cbab46c9922970191e1
owndev <69784886+owndev@users.noreply.github.com>
2025-11-17 12:07:02 +0100

    fix(google_gemini.py): Increment version to 1.6.7

--- pipelines/google/google_gemini.py
+++ pipelines/google/google_gemini.py
@@ -4,7 +4,7 @@ author: owndev, olivier-lacroix
 author_url: https://github.com/owndev/
 project_url: https://github.com/owndev/Open-WebUI-Functions
 funding_url: https://github.com/sponsors/owndev
-version: 1.6.6
+version: 1.6.7
 required_open_webui_version: 0.6.26
 license: Apache License 2.0
 description: Highly optimized Google Gemini pipeline with advanced image generation capabilities, intelligent compression, and streamlined processing workflows.
